f1e1a7a3c521b79497fe0760a38c743df775fd9e
Mathieu Débit <mathieu@debit.email>
2025-05-24 11:41:29 +0200

    Add temp and fuel gauges

--- obd-dashboard-next/src/app/page.tsx
+++ obd-dashboard-next/src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+
+import { Card, CardContent } from "@/components/ui/card"
 import RadialChart from "@/components/custom/RadialChart";
 import { ChartConfig } from "@/components/ui/chart";
 import useOBD from "@/app/hooks/useOBD";
 import Map from "@/components/custom/Map";
+import { Fuel, Thermometer } from "lucide-react";
 
 
 const chartConfig = {
@@ -15,7 +18,7 @@ export default function Home() {
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
 
-  const speed = pids.filter((command) => command.pid === 'SPEED')[0]?.value || 0;
+  const speed = pids.filter((command) => command.pid === 'SPEED')[0]?.value || 1;
   const rpm = pids.filter((command) => command.pid === 'RPM')[0]?.value || 0;
 
   const chartData = [
@@ -26,9 +29,31 @@ export default function Home() {
     <div  >
       <Map />
 
-      <div className="absolute w-full h-full p-10 border-20 border-solid border-white">
-        <div className="w-[200px]">
-          <RadialChart chartData={chartData} chartConfig={chartConfig} />
+      <div className="absolute w-full h-full p-10 border-20 border-solid border-white pointer-events-none">
+        <div className="w-100 h-100 pointer-events-auto">
+          <Card className="pb-0">
+            <CardContent className="flex items-start justify-around pb-0 w-full">
+              <div className="flex flex-col flex-none items-center">
+                <Thermometer className="mb-4"/>
+                <div className="w-9 h-3 mb-1 rounded-sm bg-red-200" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-red-200" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-red-400" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-red-400" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-red-400" />
+              </div>
+
+              <RadialChart className="mt-4" chartData={chartData} chartConfig={chartConfig} />
+
+              <div className="flex flex-col flex-none items-center">
+                <Fuel className="mb-4 ml-1" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-orange-200" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-orange-400" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-orange-400" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-orange-400" />
+                <div className="w-9 h-3 mb-1 rounded-sm bg-orange-400" />
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
--- obd-dashboard-next/src/components/custom/RadialChart.tsx
+++ obd-dashboard-next/src/components/custom/RadialChart.tsx
@@ -2,68 +2,60 @@
 
 import {
   Label,
-  PolarGrid,
   PolarRadiusAxis,
   RadialBar,
   RadialBarChart,
 } from "recharts"
 
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card"
 import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+import { cn } from "@/lib/utils"
 
-export default function RadialChart({ chartData, chartConfig }: { chartData: any[], chartConfig: ChartConfig}) {
+export default function RadialChart({ className, chartData, chartConfig }: { className: string, chartData: any[], chartConfig: ChartConfig}) {
   return (
-    <Card className="flex flex-col pb-0">
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[150px]"
-        >
-          <RadialBarChart
-            data={chartData}
-            startAngle={200}
-            endAngle={chartData[0].rpm / -30 + 180}
-            innerRadius={65}
-            outerRadius={87}
-          >
-            <RadialBar dataKey="speed" cornerRadius={10} />
-            <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                    return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                      >
-                        <tspan
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) - 12}
-                          className="fill-foreground text-4xl font-bold"
-                        >
-                          {chartData[0].speed.toLocaleString() || '0'}
-                        </tspan>
-                        <tspan
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 12}
-                          className="fill-muted-foreground"
-                        >
-                          km/h
-                        </tspan>
-                      </text>
-                    )
-                  }
-                }}
-              />
-            </PolarRadiusAxis>
-          </RadialBarChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+    <ChartContainer
+      config={chartConfig}
+      className={cn("aspect-square h-[150px]", className)}
+    >
+      <RadialBarChart
+        data={chartData}
+        startAngle={200}
+        endAngle={chartData[0].rpm / -30 + 180}
+        innerRadius={65}
+        outerRadius={87}
+      >
+        <RadialBar dataKey="speed" cornerRadius={10} />
+        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <text
+                    x={viewBox.cx}
+                    y={viewBox.cy}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                  >
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) - 12}
+                      className="fill-foreground text-4xl font-bold"
+                    >
+                      {chartData[0].speed.toLocaleString() || '1'}
+                    </tspan>
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 12}
+                      className="fill-muted-foreground"
+                    >
+                      km/h
+                    </tspan>
+                  </text>
+                )
+              }
+            }}
+          />
+        </PolarRadiusAxis>
+      </RadialBarChart>
+    </ChartContainer>
   )
 }
