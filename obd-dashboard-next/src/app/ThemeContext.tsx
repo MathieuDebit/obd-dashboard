816b5627aec88d3e2d8f85d2c624bd5c26c5ec8b
Mathieu Débit <mathieu@debit.email>
2025-05-29 17:30:49 +0200

    Update Settings UI

--- obd-dashboard-next/src/app/ThemeContext.tsx
+++ obd-dashboard-next/src/app/ThemeContext.tsx
@@ -2,8 +2,8 @@
 
 import { createContext, PropsWithChildren, useState } from "react";
 
-type Theme = "light" | "dark";
-type ThemeContext = { theme: Theme; toggleTheme: (theme: Theme) => void };
+export type Theme = "light" | "dark";
+type ThemeContext = { theme: Theme; changeTheme: (theme: Theme) => void };
 
 export const ThemeContext = createContext<ThemeContext>(
   {} as ThemeContext
@@ -11,12 +11,12 @@ export const ThemeContext = createContext<ThemeContext>(
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
   const [theme, setTheme] = useState<Theme>("light");
-  const toggleTheme = (newTheme: Theme) => {
+  const changeTheme = (newTheme: Theme) => {
     setTheme(newTheme);
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, changeTheme }}>
       {children}
     </ThemeContext.Provider>
   );
--- obd-dashboard-next/src/app/settings/page.tsx
+++ obd-dashboard-next/src/app/settings/page.tsx
@@ -1,28 +1,119 @@
 'use client';
 
 import { useContext } from "react";
-import { Switch } from "@/ui/switch";
-import { ThemeContext } from "@/app/ThemeContext";
+import { Theme, ThemeContext } from "@/app/ThemeContext";
+import { Card, CardContent } from "@/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/ui/select";
+import { Button } from "@/ui/button";
+import { Separator } from "@/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs"
 
 export default function Settings() {
-    const { theme, toggleTheme } = useContext(ThemeContext);
+    const { theme, changeTheme } = useContext(ThemeContext);
+
+    const onThemeChange = (newTheme: Theme) => {
+        changeTheme(newTheme);
+    }
 
     return (
         <div>
-            <h1 className="text-xl font-bold">Settings</h1>
-            <div>language</div>
-            <div>vehicule type</div>
-            <div>restart/shutdown</div>
-            <div>app version / update</div>
-            <div>ODB settings / restart server</div>
+            <Tabs defaultValue="general" className="w-full">
+                <TabsList className="w-full h-13 mb-5">
+                    <TabsTrigger value="general">General</TabsTrigger>
+                    <TabsTrigger value="vehicule">Vehicule</TabsTrigger>
+                    <TabsTrigger value="obd">OBD</TabsTrigger>
+                    <TabsTrigger value="about">About</TabsTrigger>
+                </TabsList>
+                <TabsContent value="general">
+                    <Card className="mb-5">
+                        <CardContent className="flex justify-between">
+                            <div>Language</div>
+                            <Select defaultValue="français">
+                                <SelectTrigger className="w-30">
+                                    <SelectValue placeholder="" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="français">Français</SelectItem>
+                                    <SelectItem value="english">English</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </CardContent>
 
-            <div>
-                Theme: 
-                <Switch
-                    checked={theme === 'dark'}
-                    onCheckedChange={() => toggleTheme(theme === 'dark' ? 'light' : 'dark')}
-                />
-            </div>
+                        <Separator />
+
+                        <CardContent className="flex justify-between">
+                            <div>Theme</div>
+                            <Select onValueChange={onThemeChange} defaultValue={theme}>
+                                <SelectTrigger className="w-30">
+                                    <SelectValue placeholder="Select theme" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="light">Light</SelectItem>
+                                    <SelectItem value="dark">Dark</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+
+                <TabsContent value="vehicule">
+                    <Card className="">
+                        <CardContent className="flex justify-between">
+                            <div>Model</div>
+                            <Select defaultValue="nissan-skyliner34gtr">
+                                <SelectTrigger className="w-55">
+                                    <SelectValue placeholder="" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Nissan</SelectLabel>
+                                        <SelectItem value="nissan-skyliner34gtr">Nissan Skyline R34 GT-R</SelectItem>
+                                        <SelectItem value="nissan-200sx">Nissan 200SX</SelectItem>
+                                    </SelectGroup>
+                                    <SelectGroup>
+                                        <SelectLabel>Toyota</SelectLabel>
+                                        <SelectItem value="toyota-supramk4">Toyota Supra MK4</SelectItem>
+                                        <SelectItem value="toyota-ae86">Toyota AE86</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                
+                <TabsContent value='obd'>
+                    <Card className="">
+                        <CardContent className="flex justify-between">
+                            <div>Restart server</div>
+                            <Button>Restart</Button>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+
+                <TabsContent value='about'>
+                    <Card className="">
+                        <CardContent className="flex justify-between">
+                            <div>App version</div>
+                            <div className="mr-1">0.1.0</div>
+                        </CardContent>
+
+                        <Separator />
+
+                        <CardContent className="flex justify-between">
+                            <div>Check for updates</div>
+                            <Button>Check</Button>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+            </Tabs>
         </div>
     )
 }
