ae5de2c79146eb34adefff47ee838a7536484fe8
Mathieu Débit <mathieu@debit.email>
2025-11-14 11:36:59 +0100

    Revert "Fix specs panel scroll"
    This reverts commit 7ef3fbda0e7f994286a9713e88dfac7ae4872479.

--- obd-dashboard-next/src/ui/scroll-area.tsx
+++ obd-dashboard-next/src/ui/scroll-area.tsx
@@ -11,12 +11,12 @@ function ScrollArea({
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
-      className={cn("relative overflow-hidden touch-pan-y select-none", className)}
+      className={cn("relative overflow-hidden touch-pan-y", className)}
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
         data-slot="scroll-area-viewport"
-        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1 overflow-auto touch-pan-y select-none"
+        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1 overflow-auto touch-pan-y select-none md:select-text"
       >
         {children}
       </ScrollAreaPrimitive.Viewport>
